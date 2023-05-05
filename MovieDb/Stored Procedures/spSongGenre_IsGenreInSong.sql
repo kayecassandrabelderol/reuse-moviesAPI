191f5449272c00a1736140558b435e8e974d65b1
John Mark P. Senening <johnmark.senening@cit.edu>
2023-05-06 02:02:23 +0800

    Rename and modify stored procedures

---
+++ MovieDb/Stored Procedures/spSongGenre_IsGenreInSong.sql
@@ -0,0 +1,11 @@
+﻿CREATE PROCEDURE [dbo].[spSongGenre_IsGenreInSong]
+	@songId int,
+	@genreId int
+AS
+BEGIN
+	SELECT COUNT(*)
+    FROM SongGenre mg
+    INNER JOIN Song m ON m.Id = mg.SongId
+    INNER JOIN Genre g ON g.Id = mg.GenreId
+    WHERE m.Id = @songId AND g.Id = @genreId
+END
---
+++ MovieDb/Stored Procedures/spSong_GetAllByArtistId.sql
@@ -0,0 +1,10 @@
+﻿CREATE PROCEDURE [dbo].[spSong_GetAllByArtistId]
+	@actorId int
+AS
+BEGIN
+	SELECT m.*
+    FROM Song m
+    INNER JOIN SongArtist ma ON ma.SongId = m.Id
+    INNER JOIN Artist a ON a.Id = ma.ArtistId
+    WHERE a.Id = @actorId
+END
