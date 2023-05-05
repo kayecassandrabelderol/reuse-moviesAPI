663f3c72fa6fdd4262f3d3aee950165430698472
John Mark P. Senening <johnmark.senening@cit.edu>
2023-05-06 02:04:34 +0800

    Rename and modify models

--- MovieApi/Models/Genre.cs
+++ MovieApi/Models/Genre.cs
@@ -4,6 +4,6 @@
     {
         public int Id { get; set; }
         public string? Name { get; set; }
-        public List<Movie> Movies { get; set; } = new List<Movie>();
+        public List<Song> Movies { get; set; } = new List<Song>();
     }
 }
