909073ceb9558865a300c1b97df79d8050281f8f
John Mark P. Senening <johnmark.senening@cit.edu>
2023-05-06 02:05:55 +0800

    Rename and modify mappings

---
+++ MovieApi/Mappings/SongMappings.cs
@@ -0,0 +1,19 @@
+﻿using AutoMapper;
+using MovieApi.Dtos.Movie;
+using MovieApi.Models;
+using System.Globalization;
+
+namespace MovieApi.Mappings
+{
+    public class SongMappings : Profile
+    {
+        public SongMappings()
+        {
+            CreateMap<Song, SongDto>()
+                .ForMember(songdto => songdto.ReleaseDate, opt => opt.MapFrom(song => song.ReleaseDate.ToString("D")));
+
+            CreateMap<SongCreationDto, Song>()
+                .ForMember(song => song.ReleaseDate, opt => opt.MapFrom(songCDto => DateTime.ParseExact(songCDto.ReleaseDate!, "M-d-yyyy", CultureInfo.InvariantCulture)));
+        }
+    }
+}
