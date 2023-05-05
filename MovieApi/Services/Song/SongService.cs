7c6080f99ee8bce6578871a3ce624608ff27a641
John Mark P. Senening <johnmark.senening@cit.edu>
2023-05-06 02:06:44 +0800

    Rename and modify services

---
+++ MovieApi/Services/Song/SongService.cs
@@ -0,0 +1,112 @@
+﻿using AutoMapper;
+using MovieApi.Contracts;
+using MovieApi.Dtos.Movie;
+using MovieApi.Models;
+using System.Globalization;
+
+namespace MovieApi.Services
+{
+    public class SongService : ISongService
+    {
+        private readonly ISongRepository _songRepository;
+        private readonly IMapper _mapper;
+
+        public SongService(ISongRepository repository, IMapper mapper)
+        {
+            _songRepository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<SongDto> CreateSong(SongCreationDto songToCreate)
+        {
+            var songModel = _mapper.Map<Song>(songToCreate);
+
+            songModel.Id = await _songRepository.Create(songModel);
+
+            return _mapper.Map<SongDto>(songModel);
+        }
+
+        public async Task<bool> DeleteSong(int id)
+        {
+            return await _songRepository.Delete(id);
+        }
+
+        public async Task<IEnumerable<SongDto>> GetAllSongs()
+        {
+            var songModels = await _songRepository.GetAll();
+
+            return _mapper.Map<IEnumerable<SongDto>>(songModels);
+        }
+
+        public async Task<IEnumerable<SongDto>> GetAllSongsByArtistId(int artistId)
+        {
+            var songModels = await _songRepository.GetAllByActorId(artistId);
+
+            return _mapper.Map<IEnumerable<SongDto>>(songModels);
+        }
+
+        public async Task<IEnumerable<SongDto>> GetAllSongsByGenreId(int genreId)
+        {
+            var songModels = await _songRepository.GetAllByGenreId(genreId);
+
+            return _mapper.Map<IEnumerable<SongDto>>(songModels);
+        }
+
+        public async Task<SongByIdDto?> GetSongById(int id)
+        {
+            var songModel = await _songRepository.GetSong(id);
+            if (songModel == null) return null;
+
+            var ret = new SongByIdDto
+            {
+                Id = songModel.Id,
+                Title = songModel.Title,
+                Duration = songModel.Duration,
+                ReleaseDate = songModel.ReleaseDate.ToString("D")
+            };
+
+            var nonNullMovies = songModel.Genres.Where(x => x != null).ToList();
+            var nonNullActors = songModel.Actors.Where(x => x != null).ToList();
+            var nonNullAwards = songModel.Awards.Where(x => x != null).ToList();
+
+            if (nonNullMovies.Any())
+            {
+                var songNames = nonNullMovies.Select(x => x.Name).ToList();
+                ret.Genres = songNames.Distinct().ToList();
+            }
+
+            if (nonNullActors.Any())
+            {
+                var artistNames = nonNullActors.Select(x => x.Name).ToList();
+                ret.Actors = artistNames.Distinct().ToList();
+            }
+
+            if (nonNullAwards.Any())
+            {
+                var awardNames = nonNullAwards.Select(x => x.Name).ToList();
+                ret.Awards = awardNames.Distinct().ToList();
+            }
+
+            return ret;
+        }
+
+        public async Task<SongDto?> GetSongOnly(int id)
+        {
+            var songModel = await _songRepository.GetSongOnly(id);
+            if (songModel == null) return null;
+
+            return _mapper.Map<SongDto>(songModel);
+        }
+
+        public async Task<bool> UpdateSong(int id, SongUpdateDto songToUpdate)
+        {
+            var songModel = new Song
+            {
+                Id = id,
+                ReleaseDate = DateTime.ParseExact(songToUpdate.ReleaseDate!, "M-d-yyyy", CultureInfo.InvariantCulture)
+            };
+
+            return await _songRepository.Update(songModel);
+        }
+    }
+}
---
+++ MovieApi/Services/SongGenre/ISongGenreService.cs
@@ -0,0 +1,29 @@
+﻿namespace MovieApi.Services
+{
+    public interface ISongGenreService
+    {
+        /// <summary>
+        /// Checks whether <paramref name="genreId"/> is in <paramref name="songId"/>
+        /// </summary>
+        /// <param name="songId"></param>
+        /// <param name="genreId"></param>
+        /// <returns>Returns true if genre exists within a song, false otherwise</returns>
+        Task<bool> IsGenreInSong(int songId, int genreId);
+
+        /// <summary>
+        /// Inserts <paramref name="genreId"/> to <paramref name="songId"/>
+        /// </summary>
+        /// <param name="songId"></param>
+        /// <param name="genreId"></param>
+        /// <returns>Returns true if insert successful, false otherwise</returns>
+        Task<bool> AddGenreInSong(int songId, int genreId);
+
+        /// <summary>
+        /// Deletes <paramref name="genreId"/> from <paramref name="songId"/>
+        /// </summary>
+        /// <param name="songId"></param>
+        /// <param name="genreId"></param>
+        /// <returns>Returns true if delete is successful, false otherwise</returns>
+        Task<bool> DeleteGenreInSong(int songId, int genreId);
+    }
+}
