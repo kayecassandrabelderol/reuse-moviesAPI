74c19c40501cc8e4da75df7e2ed68b97fc94f22d
Andre Enriquez <andre.enriquez@cit.edu>
2023-05-06 21:57:08 +0800

    Update scripts

--- MovieDb/Scripts/ArtistDataScripts.sql
+++ MovieDb/Scripts/ArtistDataScripts.sql
@@ -1,61 +1,54 @@
 ﻿
 --insert to Artist
-insert into Artist (Name, Gender, Birthday) values ('Merwin Rizziello', 'Male', '1981-08-12');
-insert into Artist (Name, Gender, Birthday) values ('Charles Sopp', 'Male', '1989-12-25');
-insert into Artist (Name, Gender, Birthday) values ('Sarina Williment', 'Female', '1985-08-26');
-insert into Artist (Name, Gender, Birthday) values ('Clarabelle Molyneaux', 'Female', '1986-01-31');
-insert into Artist (Name, Gender, Birthday) values ('Cesar Matitiaho', 'Male', '1996-11-11');
-insert into Artist (Name, Gender, Birthday) values ('Mellie O'' Hern', 'Female', '1982-11-01');
-insert into Artist (Name, Gender, Birthday) values ('Gregoor MacParland', 'Male', '1994-09-03');
-insert into Artist (Name, Gender, Birthday) values ('Ulrich Stronough', 'Male', '1996-12-23');
-insert into Artist (Name, Gender, Birthday) values ('Bartram Stannus', 'Male', '1996-06-30');
-insert into Artist (Name, Gender, Birthday) values ('Mellisa Willerstone', 'Female', '1999-07-13');
+insert into Artist (Name, Gender, Birthday) values ('IU', 'Female', '1981-08-12');
+insert into Artist (Name, Gender, Birthday) values ('Sunmi', 'Female', '1989-12-25');
+insert into Artist (Name, Gender, Birthday) values ('Taemin', 'Male', '1985-08-26');
+insert into Artist (Name, Gender, Birthday) values ('PSY', 'Male', '1986-01-31');
+insert into Artist (Name, Gender, Birthday) values ('Jackson Wang', 'Male', '1982-11-01');
 GO
 
 --insert to Genre
 insert into Genre (Name)
 values
-('Action'),
-('Comedy'),
-('Drama')
+('Genre Sample1'),
+('Genre Sample2'),
+('Genre Sample3'),
+('Genre Sample4'),
+('Genre Sample5')
 GO
 
 --insert to Song
-insert into Song (Title, Duration, ReleaseDate) values ('SAMPLE2', 255, '2016-11-20');
-insert into Song (Title, Duration, ReleaseDate) values ('SAMPLE1', 255, '2023-11-20');
+insert into Song (Title, Duration, ReleaseDate) values ('sample song 1', 255, '2016-11-20');
+insert into Song (Title, Duration, ReleaseDate) values ('sample song 2', 255, '2023-11-20');
+insert into Song (Title, Duration, ReleaseDate) values ('sample song 3', 255, '2023-11-20');
+insert into Song (Title, Duration, ReleaseDate) values ('sample song 4', 255, '2023-11-20');
+insert into Song (Title, Duration, ReleaseDate) values ('sample song 5', 255, '2023-11-20');
 GO
 
-/* --insert mocks to Award
-insert into Award (Name, Year, SongId) values ('cras pellentesque volutpat dui', 2020, 10);
-insert into Award (Name, Year, SongId) values ('in hac habitasse platea', 2015, 10);
-insert into Award (Name, Year, SongId) values ('habitasse platea dictumst maecenas ut', 2010, 3);
-insert into Award (Name, Year, SongId) values ('diam erat fermentum', 2004, 10);
-insert into Award (Name, Year, SongId) values ('quis libero nullam sit amet', 2002, 7);
-GO */
+--insert to Award
+insert into Award (Name, Year, SongId) values ('sample award 1', 2021, 1);
+insert into Award (Name, Year, SongId) values ('sample award 2', 2022, 2);
+insert into Award (Name, Year, SongId) values ('sample award 3', 2023, 3);
+insert into Award (Name, Year, SongId) values ('sample award 4', 2024, 4);
+insert into Award (Name, Year, SongId) values ('sample award 5', 2025, 5);
+GO
 
-/*--SongArtists
+--SongArtists
 insert into SongArtist(SongId, ArtistId)
 values
-(1,1), (1,2),
-(2,2), (2,3),
-(3,3), (3,4),
-(4,4), (4,5),
-(5,5), (5,6),
-(6,6), (6,7),
-(7,7), (7,8),
-(8,8), (8,9),
-(9,9)
+(1,1),
+(2,2),
+(3,3),
+(4,4),
+(5,5)
 GO
 
 --SongGenres
 insert into SongGenre (SongId, GenreId)
 values
 (1,1),
-(2,1), (2,2),
-(3,2),
-(4,2), (4,3),
-(5,1),
-(6,1), (6,2),
-(7,2),
-(8,2), (8,3),
-(9,3)*/
+(2,2),
+(3,3),
+(4,4),
+(5,5)
+GO
